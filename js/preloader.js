9441fc1fd470797d23852c1714ad357796241f55
Marina <maryna_kuzmiankova@mail.ru>
2022-12-26 15:55:14 +0300

    fix path to img

---
+++ js/preloader.js
@@ -0,0 +1,14 @@
+'use strict';
+const preloader = () => {
+    const preloader = document.querySelector('.preloder');
+
+    preloader.classList.add('active');
+    
+    setTimeout(() => {
+        preloader.classList.remove('active');
+    }, 500);
+}
+
+preloader();
+
+
